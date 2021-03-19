2bc802a9d979bebb2d25cb35272305465e2c92bf
mayra@mumuki.org <mayra@mumuki.org>
2021-03-19 14:29:08 +0000

    Mumuki Export on 2021-03-19 14:29:08 +0000

--- 00007_Repetir letras/test..js
+++ 00007_Repetir letras/test..js
@@ -1,5 +1,9 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it("Si hacemos repetirLetras('hola', 2) debería retornar hhoollaa", function() {
+  let resultado = repetirLetras('hola', 2)
+  assert.equal(resultado, 'hhoollaa',`repetirLetras('hola', 2) debería retornar 'hhoollaa' pero fue ${resultado}`);
+})
+
+it("Si hacemos repetirLetras('ada', 3) debería retornar aaadddaaa", function() {
+  let resultado = repetirLetras('ada', 3)
+  assert.equal(resultado, 'aaadddaaa',`repetirLetras('ada', 3) debería retornar 'aaadddaaa' pero fue ${resultado}`);
 })
