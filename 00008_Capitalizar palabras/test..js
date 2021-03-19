3b15e3151734f510f6111bf221f0fd5c3061a65f
mayra@mumuki.org <mayra@mumuki.org>
2021-03-19 14:38:09 +0000

    Mumuki Export on 2021-03-19 14:38:09 +0000

--- 00008_Capitalizar palabras/test..js
+++ 00008_Capitalizar palabras/test..js
@@ -1,5 +1,9 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it("Si hacemos capitalizarPalabras('Esto es un título') debería retornar 'Esto Es Un Título'", function() {
+  let resultado = capitalizarPalabras('Esto es un título')
+  assert.equal(resultado, 'Esto Es Un Título',`capitalizarPalabras('Esto es un título') debería retornar 'Esto Es Un Título' pero fue ${resultado}`);
+})
+
+it("Si hacemos capitalizarPalabras('había una vez') debería retornar 'Había Una Vez'", function() {
+  let resultado = capitalizarPalabras('había una vez')
+  assert.equal(resultado, 'Había Una Vez',`capitalizarPalabras('había una vez') debería retornar 'Había Una Vez' pero fue ${resultado}`);
 })
