ffdaa9cb1cfa3548cf213b3b9484a34605f17996
mayra@mumuki.org <mayra@mumuki.org>
2021-03-19 14:02:22 +0000

    Mumuki Export on 2021-03-19 14:02:22 +0000

--- 00001_Obtener indice/test..js
+++ 00001_Obtener indice/test..js
@@ -1,5 +1,9 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it("Si hacemos obtenerIndice(12, [5, 7, 12, 34, 54, 2, 12]) debería retornar 2", function() {
+  let resultado = obtenerIndice(12, [5, 7, 12, 34, 54, 2, 12])
+  assert.equal(resultado, 2,`obtenerIndice(12, [5, 7, 12, 34, 54, 2, 12]) debería retornar 2 pero fue ${resultado}`);
+})
+
+it("Si hacemos obtenerIndice(83, [5, 7, 12, 34, 54, 2, 12]) debería retornar -1", function() {
+  let resultado = obtenerIndice(83, [5, 7, 12, 34, 54, 2, 12])
+  assert.equal(resultado, -1,`obtenerIndice(83, [5, 7, 12, 34, 54, 2, 12]) debería retornar -1 pero fue ${resultado}`);
 })
