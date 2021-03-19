75627281ffa26f9470b597560cfe83808666792e
mayra@mumuki.org <mayra@mumuki.org>
2021-03-19 14:25:31 +0000

    Mumuki Export on 2021-03-19 14:25:31 +0000

--- 00005_Invertir/test..js
+++ 00005_Invertir/test..js
@@ -1,5 +1,9 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it("Si hacemos invertir([1, 2, 3])  debería retornar [3, 2, 1]", function() {
+  let resultado = invertir([1, 2, 3]) 
+  assert.equal(resultado, [3, 2, 1],`invertir([1, 2, 3]) debería retornar [3, 2, 1] pero fue ${resultado}`);
+})
+
+it("Si hacemos invertir([5, 7, 99, 34, 54, 2, 12])  debería retornar [12, 2, 54, 34, 99, 7, 5]", function() {
+  let resultado = invertir([5, 7, 99, 34, 54, 2, 12]) 
+  assert.equal(resultado, [12, 2, 54, 34, 99, 7, 5],`invertir([5, 7, 99, 34, 54, 2, 12]) debería retornar [12, 2, 54, 34, 99, 7, 5] pero fue ${resultado}`);
 })
--- 00006_Remover duplicados/test..js
+++ 00006_Remover duplicados/test..js
@@ -1,5 +1,9 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it("Si hacemos removerDuplicados([1, 1, 1]) debería retornar [1]", function() {
+  let resultado = removerDuplicados([1, 1, 1])
+  assert.equal(resultado, [1],`removerDuplicados([1, 1, 1]) debería retornar [1] pero fue ${resultado}`);
+})
+
+it("Si removerDuplicados([1, 1, 2, 2, 3, 3]) debería retornar [1, 2 ,3]", function() {
+  let resultado = removerDuplicados([1, 1, 2, 2, 3, 3])
+  assert.equal(resultado, [1, 2 ,3],`removerDuplicados([1, 1, 2, 2, 3, 3]) debería retornar [1, 2 ,3] pero fue ${resultado}`);
 })
