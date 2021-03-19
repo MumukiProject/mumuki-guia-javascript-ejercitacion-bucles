b8a8f66bb16a1165120f39e947cab9d072a89af2
mayra@mumuki.org <mayra@mumuki.org>
2021-03-19 15:42:30 +0000

    Mumuki Export on 2021-03-19 15:42:30 +0000

--- 00004_Crear cuenta regresiva/test..js
+++ 00004_Crear cuenta regresiva/test..js
@@ -1,6 +1,6 @@
 it("Si hacemos crearCuentaRegresiva(3) debería retornar [3, 2, 1, 0]", function() {
   let resultado = crearCuentaRegresiva(3)
-  assert.equal(resultado, 9[3, 2, 1, 0],`crearCuentaRegresiva(3) debería retornar [3, 2, 1, 0] pero fue ${resultado}`);
+  assert.equal(resultado, [3, 2, 1, 0],`crearCuentaRegresiva(3) debería retornar [3, 2, 1, 0] pero fue ${resultado}`);
 })
 
 it("Si hacemos crearCuentaRegresiva(5) debería retornar [5, 4, 3, 2, 1, 0]", function() {
