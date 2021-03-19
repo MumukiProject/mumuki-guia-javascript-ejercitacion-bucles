e0b68ff09463dcf0b20d2ced20fd86ba8bbc9316
mayra@mumuki.org <mayra@mumuki.org>
2021-03-19 14:12:05 +0000

    Mumuki Export on 2021-03-19 14:12:05 +0000

--- 00003_Sumar impares hasta/test..js
+++ 00003_Sumar impares hasta/test..js
@@ -1,5 +1,14 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it("Si hacemos sumarImparesHasta(5) debería retornar 9", function() {
+  let resultado = sumarImparesHasta(5)
+  assert.equal(resultado, 9,`sumarImparesHasta(5) debería retornar 9 pero fue ${resultado}`);
+})
+
+it("Si hacemos sumarImparesHasta(13) debería retornar 49", function() {
+  let resultado = sumarImparesHasta(13)
+  assert.equal(resultado, 49,`sumarImparesHasta(13) debería retornar 49 pero fue ${resultado}`);
+})
+
+it("Si hacemos sumarImparesHasta(47) debería retornar 576", function() {
+  let resultado = sumarImparesHasta(47)
+  assert.equal(resultado, 576,`sumarImparesHasta(47) debería retornar 576 pero fue ${resultado}`);
 })
