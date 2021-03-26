4278d0e943e2fcb661c1c93e0a9a13bf4693a7bc
mayra@mumuki.org <mayra@mumuki.org>
2021-03-26 18:10:50 +0000

    Mumuki Export on 2021-03-26 18:10:50 +0000

--- 00002_Repetir/test..js
+++ 00002_Repetir/test..js
@@ -1,10 +1,10 @@
 it("Si hacemos repetir('lovelace', 3) debería retornar ['lovelace', 'lovelace', 'lovelace']", function() {
   let resultado = repetir('lovelace', 3)
-  assert.equal(resultado, ['lovelace', 'lovelace', 'lovelace'],`repetir('lovelace', 3) debería retornar ['lovelace', 'lovelace', 'lovelace'] pero fue ${resultado}`);
+  assert.deepEqual(resultado, ['lovelace', 'lovelace', 'lovelace'],`repetir('lovelace', 3) debería retornar ['lovelace', 'lovelace', 'lovelace'] pero fue ${resultado}`);
 })
 
 it("Si hacemos repetir('a', 5) debería retornar ['a', 'a', 'a', 'a', 'a']", function() {
   let resultado = repetir('a', 5)
-  assert.equal(resultado, ['a', 'a', 'a', 'a', 'a'],`repetir('a', 5) debería retornar ['a', 'a', 'a', 'a', 'a'] pero fue ${resultado}`);
+  assert.deepEqual(resultado, ['a', 'a', 'a', 'a', 'a'],`repetir('a', 5) debería retornar ['a', 'a', 'a', 'a', 'a'] pero fue ${resultado}`);
 })
 
