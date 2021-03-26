7e2aaa86ab36c8e7ceafbf6c51eeb60acadbf77f
mayra@mumuki.org <mayra@mumuki.org>
2021-03-26 18:31:37 +0000

    Mumuki Export on 2021-03-26 18:31:37 +0000

--- 00006_Remover duplicados/test..js
+++ 00006_Remover duplicados/test..js
@@ -1,9 +1,9 @@
 it("Si hacemos removerDuplicados([1, 1, 1]) debería retornar [1]", function() {
   let resultado = removerDuplicados([1, 1, 1])
-  assert.equal(resultado, [1],`removerDuplicados([1, 1, 1]) debería retornar [1] pero fue ${resultado}`);
+  assert.deepEqual(resultado, [1],`removerDuplicados([1, 1, 1]) debería retornar [1] pero fue ${resultado}`);
 })
 
 it("Si removerDuplicados([1, 1, 2, 2, 3, 3]) debería retornar [1, 2 ,3]", function() {
   let resultado = removerDuplicados([1, 1, 2, 2, 3, 3])
-  assert.equal(resultado, [1, 2 ,3],`removerDuplicados([1, 1, 2, 2, 3, 3]) debería retornar [1, 2 ,3] pero fue ${resultado}`);
+  assert.deepEqual(resultado, [1, 2 ,3],`removerDuplicados([1, 1, 2, 2, 3, 3]) debería retornar [1, 2 ,3] pero fue ${resultado}`);
 })
