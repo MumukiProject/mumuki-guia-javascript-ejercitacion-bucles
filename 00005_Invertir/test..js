8cb54578187669a43b41b64225452b4ef2196226
mayra@mumuki.org <mayra@mumuki.org>
2021-03-26 18:26:57 +0000

    Mumuki Export on 2021-03-26 18:26:57 +0000

--- 00005_Invertir/test..js
+++ 00005_Invertir/test..js
@@ -1,9 +1,9 @@
 it("Si hacemos invertir([1, 2, 3])  debería retornar [3, 2, 1]", function() {
   let resultado = invertir([1, 2, 3]) 
-  assert.equal(resultado, [3, 2, 1],`invertir([1, 2, 3]) debería retornar [3, 2, 1] pero fue ${resultado}`);
+  assert.deepEqual(resultado, [3, 2, 1],`invertir([1, 2, 3]) debería retornar [3, 2, 1] pero fue ${resultado}`);
 })
 
 it("Si hacemos invertir([5, 7, 99, 34, 54, 2, 12])  debería retornar [12, 2, 54, 34, 99, 7, 5]", function() {
   let resultado = invertir([5, 7, 99, 34, 54, 2, 12]) 
-  assert.equal(resultado, [12, 2, 54, 34, 99, 7, 5],`invertir([5, 7, 99, 34, 54, 2, 12]) debería retornar [12, 2, 54, 34, 99, 7, 5] pero fue ${resultado}`);
+  assert.deepEqual(resultado, [12, 2, 54, 34, 99, 7, 5],`invertir([5, 7, 99, 34, 54, 2, 12]) debería retornar [12, 2, 54, 34, 99, 7, 5] pero fue ${resultado}`);
 })
