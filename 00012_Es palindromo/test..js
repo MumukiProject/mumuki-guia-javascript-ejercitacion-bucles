e83a3602fa780adca5d247fc7df9b8a223695cfe
mayra@mumuki.org <mayra@mumuki.org>
2021-03-29 18:25:07 +0000

    Mumuki Export on 2021-03-29 18:25:07 +0000

--- 00012_Es palindromo/test..js
+++ 00012_Es palindromo/test..js
@@ -1,9 +1,9 @@
 it("Si hacemos esPalindromo('ada') debería retornar true", function() {
   let resultado = esPalindromo('ada')
-  assert.equal(resultado, true,`esPalindromo('ada') debería retornar true pero fue ${resultado}`);
+  assert.deepEqual(resultado, true,`esPalindromo('ada') debería retornar true pero fue ${resultado}`);
 })
 
 it("Si hacemos esPalindromo('lovelace') debería retornar false", function() {
   let resultado = esPalindromo('lovelace')
-  assert.equal(resultado, false,`esPalindromo('lovelace') debería retornar false pero fue ${resultado}`);
+  assert.deepEqual(resultado, false,`esPalindromo('lovelace') debería retornar false pero fue ${resultado}`);
 })
